7bfc7e9a77d72f17ec7106174e1b37c9c39b8f4a
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2023-12-13 18:03:09 +0700

    coba fitur daisyUI

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <hero />
+    </div>
+  </div>
+</template>
---
+++ components/navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar top-0 fixed px-20 z-20 bg-gray-800">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <div class="dropdown dropdown-bottom dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-square btn-ghost m-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
